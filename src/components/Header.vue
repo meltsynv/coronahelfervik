253c6498453865f973dc1f7cd8db4b2c9b80c026
Viktor Meltsyn <meltsynv@yahoo.com>
2020-04-24 01:54:16 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-header class="nav">
+    <q-toolbar>
+      <q-toolbar-title>
+        <router-link class="logo-link" to="/" exact>
+          <img src="../assets/CoronaHelferLogo.svg">
+        </router-link>
+      </q-toolbar-title>
+      <q-tabs shrink stretch align="right">
+        <q-route-tab to="/helfen" label="Helfen"/>
+        <q-route-tab to="/hilfe erhalten" label="Hilfe erhalten"/>
+        <q-route-tab to="/informationen" label="Infos"/>
+        <q-route-tab to="/jobs" label="Jobs"/>
+      </q-tabs>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<style lang="scss">
+.nav{
+    background-color: $secondary;
+    .logo-link{
+      img{
+        width: auto;
+        height: 55px;
+        padding: 10px;
+      }
+    }
+}
+</style>
---
+++ src/pages/Helfen.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="wrapper">
+    <Regionalesuche class="q-mb-xl" />
+    <Seperator/>
+    <BlockCard/>
+    <BlockCard/>
+    <BlockCard/>
+    <BlockCard/>
+  </q-page>
+</template>
+
+<script>
+import Regionalesuche from "components/Regionalesuche";
+import BlockCard from "components/BlockCard";
+import Seperator from 'components/Seperator';
+export default {
+  components: {
+    Regionalesuche,
+    BlockCard,
+    Seperator
+  }
+};
+</script>
+
+<style lang="scss">
+.wrapper {
+  width: 65%;
+  margin: 0 auto;
+}
+</style>
